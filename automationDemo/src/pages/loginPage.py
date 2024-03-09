e654714c240f5a6b5cc025bfcd4239b7bf77c682
momtazrasel <momtazrasel8@gmail.com>
2024-03-09 17:43:20 +0600

    Dropdown function is up

--- automationDemo/src/pages/loginPage.py
+++ automationDemo/src/pages/loginPage.py
@@ -5,6 +5,7 @@ class LoginPageClass:
     def __init__(self, page):
         self.page = page
         self._dropdownButton = page.locator("(//select[@class='tls-input-select'])[1]")
+        self._dropdownOption = page.locator("(//option[@value='gb'])[1]")
         # self._dropdownButton = page.locator("(//h5[normalize-space()='Elements'])[1]")
         self._username = page.locator("")
         self._password = page.locator("")
@@ -12,3 +13,14 @@ class LoginPageClass:
     def click_dropdown_button(self):
         self._dropdownButton.click()
         time.sleep(2)
+        self._dropdownOption.click()
+        time.sleep(2)
+        # dropdown = self._dropdownButton
+        # time.sleep(2)
+        # dropdown.select_option("gb")
+
+    def click_dropdown_buttons(self, option_value):
+        self._dropdownButton.click()
+        time.sleep(2)  # You may want to replace this sleep with proper waiting logic
+        dropdown = self._dropdownButton
+        dropdown.select_option({'label': option_value})
--- automationDemo/tests/login/test.py
+++
@@ -1,5 +0,0 @@
-from playwright.sync_api import Page
-
-
-def test_has_title(page: Page):
-    page.goto("https://visas-fr.tlscontact.com/login")
